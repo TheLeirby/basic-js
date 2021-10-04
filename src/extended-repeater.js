d7a93e76960620d4dec54681b00f51fe87a0201c
Vasiliy <88965041+TheLeirby@users.noreply.github.com>
2021-10-04 23:05:06 +0300

    fix: Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,40 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+   if (options.additionSeparator === undefined) {
+     options.additionSeparator = "|";
+   }
+  if (options.separator === undefined) {
+    options.separator = "+";
+  }
+  str = String(str);
+
+  let addition = String(options.addition);
+  if (options.addition === undefined) {
+    addition = "";
+  }
+  let addElse = addition;
+  let string = str;
+
+  let newArr='';
+
+  if (1< options.additionRepeatTimes) {    
+    for (let i = 0; i < options.additionRepeatTimes - 1; i++){
+      addElse = addElse + options.additionSeparator + addition;
+      newArr=newArr+' | '+addElse;
+    }
+  }
+  newArr=newArr+'~~';
+  if (1 < options.repeatTimes) {
+    string = string + addElse;
+    for (let y = 0; y < options.repeatTimes - 1; y++) {
+      string = string+options.separator + str + addElse;
+      newArr=newArr+ ' || '+string;
+    }
+  } else {
+    string = string + addElse;
+  }
+
+  return string;
 }
